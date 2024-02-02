8da4777dbd8086f3246fc47d43b5ea8f815b2c8e
David Yeh <davidmilk880330@gmail.com>
2024-02-02 21:11:39 +0800

    add new files

--- password.py
+++ password.py
@@ -1,9 +1,16 @@
-i=2
-while i >= 0:
-	password = input('請輸入密碼:')
-	if password == 'a123456':
+password = 'a123456'
+i = 3 
+while i > 0:
+	i = i - 1
+	pwd = input('請輸入密碼:')
+	if pwd == password:
 		print('登入成功!')
 		break
 	else:
-		print('密碼錯誤! 還有', i , '次機會')
-	i=i-1
+		print('密碼錯誤!')
+		if i > 0:
+			print('還有', i, '次機會')
+		else:
+			print('沒機會嘗試了!要索 帳號了拉!')
+
+
